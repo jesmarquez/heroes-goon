e0d3b67511b7e45bda3c0c935df7989dc94e3c3d
Dell_JESMQZ <jesmqz@gmail.com>
2026-05-07 21:35:35 -0400

    Axios ENV API

---
+++ README.md
@@ -0,0 +1,7 @@
+# Heroes app
+
+## Levantar desarrollo
+
+1. Clonar
+2. Editar .env
+3. Ejecutar npm install
---
+++ src/heroes/pages/home/HomePage.tsx
@@ -0,0 +1,84 @@
+import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
+import { CustomJumbotron } from '@/components/custom/CustomJumbotron';
+import { HeroStats } from '@/heroes/components/HeroStats';
+import { HeroGrid } from '@/heroes/components/HeroGrid';
+import { useEffect, useState } from 'react';
+import { CustomPagination } from '@/components/custom/CustomPagination';
+import { CustomBreadcrumbs } from '@/components/custom/CustomBreadcrumbs';
+import { getHeroesByPage } from '@/heroes/actions/get-heroes-by-page.action';
+
+export const HomePage = () => {
+  const [activeTab, setActiveTab] = useState<
+    'all' | 'favorites' | 'heroes' | 'villains'
+  >('all');
+
+  useEffect(() => {
+    getHeroesByPage().then((heroes) => {
+      console.log({ heroes });
+    });
+  }, []);
+  return (
+    <>
+      <>
+        {/* Header */}
+        <CustomJumbotron
+          title="Universo de SuperHéroes"
+          description="Descubre, explora y administra super héroes y villanos"
+        />
+
+        <CustomBreadcrumbs currentPage="Super Héroes" />
+
+        {/* Stats Dashboard */}
+        <HeroStats />
+
+        {/* Tabs */}
+        <Tabs value={activeTab} className="mb-8">
+          <TabsList className="grid w-full grid-cols-4">
+            <TabsTrigger value="all" onClick={() => setActiveTab('all')}>
+              All Characters (16)
+            </TabsTrigger>
+            <TabsTrigger
+              value="favorites"
+              className="flex items-center gap-2"
+              onClick={() => setActiveTab('favorites')}
+            >
+              Favorites (3)
+            </TabsTrigger>
+            <TabsTrigger value="heroes" onClick={() => setActiveTab('heroes')}>
+              Heroes (12)
+            </TabsTrigger>
+            <TabsTrigger
+              value="villains"
+              onClick={() => setActiveTab('villains')}
+            >
+              Villains (2)
+            </TabsTrigger>
+          </TabsList>
+
+          <TabsContent value="all">
+            {/* Mostrar todos los personajes */}
+            <HeroGrid />
+          </TabsContent>
+          <TabsContent value="favorites">
+            {/* Mostrar todos los personajes favoritos */}
+            <h1>Favoritos!!!</h1>
+            <HeroGrid />
+          </TabsContent>
+          <TabsContent value="heroes">
+            {/* Mostrar todos los héroes */}
+            <h1>Héroes</h1>
+            <HeroGrid />
+          </TabsContent>
+          <TabsContent value="villains">
+            {/* Mostrar todos los Villanos */}
+            <h1>Villanos</h1>
+            <HeroGrid />
+          </TabsContent>
+        </Tabs>
+
+        {/* Pagination */}
+        <CustomPagination totalPages={8} />
+      </>
+    </>
+  );
+};
