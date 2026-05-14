cbc8e36ff1a1813fc2f011c4ce5f880fd527dd4c
Dell_JESMQZ <jesmqz@gmail.com>
2026-05-14 11:06:54 -0400

    Navegacion manual dentro de tarjetas

--- src/heroes/components/HeroGridCard.tsx
+++ src/heroes/components/HeroGridCard.tsx
@@ -5,19 +5,28 @@ import { Button } from '@/components/ui/button';
 import { Card, CardHeader, CardContent } from '@/components/ui/card';
 import { Progress } from '@/components/ui/progress';
 import type { Hero } from '../types/hero.interface';
+import { useNavigate } from 'react-router';
 
 interface Props {
   hero: Hero
 }
 
 export const HeroGridCard = ({ hero }: Props) => {
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/heroes/${hero.slug}`);
+  }
+
   return (
     <Card className="group overflow-hidden hover:shadow-xl transition-all duration-300 hover:-translate-y-1 bg-gradient-to-br from-white to-gray-50">
-      <div className="relative h-64 overflow-hidden">
+      <div className="relative h-64">
         <img
           src={ hero.image }
           alt="Superman"
-          className="object-cover transition-all duration-500 group-hover:scale-110"
+          className="object-cover transition-all duration-500 group-hover:scale-110 absolute top-[-30px] w-full h-[410px]"
+          onClick={handleClick}
         />
 
         {/* Status indicator */}
@@ -68,7 +77,7 @@ export const HeroGridCard = ({ hero }: Props) => {
         </Button>
       </div>
 
-      <CardHeader className="pb-3">
+      <CardHeader className="py-3 z-10 bg-gray-100/50 backdrop-blur-sm relative top-1 group-hover:top-[-10px] transition-all duration-300">
         <div className="flex justify-between items-start">
           <div className="space-y-1">
             <h3 className="font-bold text-lg leading-tight">{ hero.alias }</h3>
@@ -127,7 +136,7 @@ export const HeroGridCard = ({ hero }: Props) => {
 
             {
               hero.powers.slice(0,3).map (power => 
-                <Badge variant="outline" className="text-xs">
+                <Badge key={ power } variant="outline" className="text-xs">
                   { power }
                 </Badge>
               )
