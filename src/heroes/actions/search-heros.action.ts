fe2a58b1c027eaa90836a900006d46cd9d6dcf8c
Dell_JESMQZ <jesmqz@gmail.com>
2026-05-21 09:14:06 -0400

    filtros avanzados

---
+++ src/heroes/actions/search-heros.action.ts
@@ -0,0 +1,38 @@
+import { heroApi } from "../api/hero.api";
+import type { Hero } from "../types/hero.interface";
+
+interface Options {
+  name?: string;
+  team?: string;
+  category?: string;
+  universe?: string;
+  status?: string;
+  strength?: string;
+}
+
+const VITE_API_URL = import.meta.env.VITE_API_URL;
+
+export const searchHeroesAction = async(options: Options = {}) => {
+  const { name, team, category, universe, status, strength } = options;
+
+  if (!name && !team && !category && !universe && !status && !strength) {
+    return [];
+  }
+
+  const { data } = await heroApi.get<Hero[]>('/heroes/search' , {
+    params: {
+      name,
+      team,
+      category,
+      universe,
+      status,
+      strength
+    }
+  });
+
+  console.log(data);
+  return data.map(hero => ({
+    ...hero,
+    image: `${VITE_API_URL}/images/${hero.image}`
+  }));
+}
--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -2,8 +2,25 @@ import { CustomJumbotron } from '@/components/custom/CustomJumbotron';
 import { HeroStats } from '@/heroes/components/HeroStats';
 import { SearchControls } from './ui/SearchControls';
 import { CustomBreadcrumbs } from '@/components/custom/CustomBreadcrumbs';
+import { useQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'react-router';
+import { searchHeroesAction } from '@/heroes/actions/search-heros.action';
+import { HeroGrid } from '@/heroes/components/HeroGrid';
 
 export const SearchPage = () => {
+
+  const [ searchParams ] = useSearchParams();
+  const name = searchParams.get('name') ?? undefined;
+  const strength = searchParams.get('strength') ?? undefined;
+
+  console.log(name);
+  const { data: heroes = []} = useQuery({
+    queryKey: ['search', { name, strength }],
+    queryFn: () => searchHeroesAction({ name, strength }),
+    staleTime: 1000 * 60 * 5,
+  });
+  console.log(heroes);
+
   return (
     <>
       <CustomJumbotron
@@ -25,6 +42,8 @@ export const SearchPage = () => {
 
       {/* Filter and search */}
       <SearchControls />
+
+      <HeroGrid heroes={ heroes }/>
     </>
   );
 };
